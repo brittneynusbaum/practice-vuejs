70926e2923f20ef47dc8427381fc27dadf8a38e7
brittneynusbaum <brittney.nusbaum@gmail.com>
2022-04-01 19:05:58 -0700

    adds todos html/js & get request

--- todos.js
+++ todos.js
@@ -5,26 +5,21 @@ var App = {
     return {
       message: "Hello Vue!",
       todos: [
-        {
-          "userId": 1,
-          "id": 1,
-          "title": "delectus aut autem",
-          "completed": false
-        },
-        {
-          "userId": 1,
-          "id": 2,
-          "title": "quis ut nam facilis et officia qui",
-          "completed": false
-        },
-        {
-          "userId": 1,
-          "id": 3,
-          "title": "fugiat veniam minus",
-          "completed": false
-        },
+        
       ],
     };
+  },
+  
+  methods: {
+    // ...
+    loadTodos: function() { 
+      axios
+        .get("https://jsonplaceholder.typicode.com/todos")
+        .then(response => {
+          console.log(response.data);
+          this.todos = response.data;
+        });
+    }
   }
 };
 
